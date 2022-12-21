7a281fa03ef898864d524a5e09c0181b5527fa0d
OlaoyeVic <you@example.com>
2022-12-21 20:01:51 +0100

    implemented the popular movie section

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com', 'image.tmdb.org'],
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: ['lh3.googleusercontent.com'],
+        hostname: 'image.tmdb.org',
         port: '',
         pathname: '/image/upload/**',
       },
--- pages/browse/index.tsx
+++ pages/browse/index.tsx
@@ -5,23 +5,29 @@ import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query"
 import { fetchMovies } from "../api/browse"
 import Image from "next/image"
 import Link from "next/link"
-import { Key, useState } from "react"
+import { Key, useEffect, useState } from "react"
 
-// export const getServerSideProps = withPageAuthRequired()
 
-// export async function getServerSideProps() {
-//     withPageAuthRequired()
-//     const queryClient = new QueryClient()
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
 
-//     await queryClient.prefetchQuery(['movies'], fetchMovies)
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
-//     return {
-//         props: {
-//             dehydratedState: dehydrate(queryClient)
-//         }
-//     }
-// }
-// export const getServerSideProps = withPageAuthRequired()
+
 export const getServerSideProps: GetServerSideProps = async () => {
 
     const queryClient = new QueryClient()
@@ -36,42 +42,70 @@ export const getServerSideProps: GetServerSideProps = async () => {
 }
 
 const BrowseMovies: NextPage = () => {
-    // const [fetchedResults, setFetchedResults] = useState([])
+    const imageUrl = "https://image.tmdb.org/t/p/w500"
     const { data } = useQuery({ queryKey: ['movies'], queryFn: fetchMovies})
     console.log(data)
-    return data
 
     
     return (
         <div className="browse">
             <div className="mobile-browse-container">
-                <Navbar />
-                <div className="movie sections">
-                    <div className="movie-trailer-section"></div>
+                    <Navbar />
+                <div className="movie-sections">
+                    <div className="movie-trailer-section">
+                    </div>
                     <div className="movie-categories">
-                        <p>Popular Movies</p>
+                        <h2>Popular Movies</h2>
                         <div className="popular-movies-section">
-                            {data?.results.map((movies: string | any, index: number)=> (
-                                <>
-                                    <Link href='' passHref>
-                                        <a key={index}>
+                            {data?.results?.map((result: string | any, index: number) => ( 
+                                    <Link href='' passHref >
+                                        <figure key={index} data-content={result?.original_title}>
                                             <Image 
-                                             src={movies?.poster_path} 
-                                             alt={movies?.title} 
-                                             width={30} 
-                                             height={30}
+                                                // loader={() => imageUrl + result?.poster_path}
+                                                src={"https://image.tmdb.org/t/p/w500" + result.backdrop_path}
+                                                alt={result?.title} 
+                                                width={148} 
+                                                height={160}
+                                                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+                                                placeholder="blur"
+                                                style={{
+                                                    borderRadius: '5px',
+                                                }}
                                             />
-                                        </a>
+                                        </figure>
                                     </Link>
-                                    <p>{movies.title}</p>
-                                </>
                             ))}
                         </div>
+                        <h2></h2>
                     </div>
                 </div>
             </div>
             <div className="desktop-browse-container">
                 <Navbar />
+                <div className="desktop-movie-sections">
+                    <div className="desktop-trailer-section"></div>
+                    <div className="desktop-popular-movie-category">
+                        <p>Popular Movies</p>
+                            {data?.results?.map((result: string | any, index: number) => (
+                                    <Link href='' passHref>
+                                        <figure key={index}>
+                                            <Image
+                                                src={"https://image.tmdb.org/t/p/w500" + result?.backdrop_path}
+                                                alt={result?.title} 
+                                                width={237} 
+                                                height={134}
+                                                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+                                                placeholder="blur"
+                                                style={{
+                                                    borderRadius: '5px'
+                                                }}
+                                            />
+                                            <figcaption>{result?.title}</figcaption>
+                                        </figure>
+                                    </Link>
+                            ))}
+                    </div>
+                </div>
             </div>
         </div>
     )
